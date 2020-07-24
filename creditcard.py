c51e52e0bc3d4c50e87c8fb0e0f3513ab9afe2f2
rameeyboy96 <ramachandranravi96@gmail.com>
2020-07-23 19:05:25 -0700

    adding files

---
+++ creditcard.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#import the dataset
+df=pd.read_csv('creditcard.csv')
